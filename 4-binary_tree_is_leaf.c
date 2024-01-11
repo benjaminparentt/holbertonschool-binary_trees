696a034520e7eb8f3f5e41537e5706951e0c1400
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-11 14:10:04 -0800

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - check if @node is a leaf
+ * @node: a tree
+ * Return: 1(node is a leaf) or 0(node is not a leaf)
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && (node->left != NULL || node->right != NULL))
+		return  (0);
+	return (1);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -34,5 +34,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif
