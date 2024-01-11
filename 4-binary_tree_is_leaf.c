e748c51a7a905414dafb9fa73998ad554d3257a0
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-11 14:16:35 -0800

    fix task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node != NULL && (node->left != NULL || node->right != NULL))
-		return  (0);
-	return (1);
+	if (node != NULL && node->left != NULL && node->right != NULL)
+		return  (1);
+	return (0);
 }
