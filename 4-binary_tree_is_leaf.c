b40bb51cdc9b8c5354ce7a0b6f4e6686bfb3161d
ArseneGiriteka <giritekaaarsene@gmail.com>
2024-01-11 14:26:13 -0800

    adding files

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,8 +7,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if ((node != NULL && (node->left != NULL || node->right != NULL)) &&
-			node->parent != NULL)
+	if (node != NULL && node->left == NULL && node->right == NULL)
 		return  (1);
 	return (0);
 }
